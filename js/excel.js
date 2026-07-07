ae74e4e5efad5be8e0144caa390af097925b749f
Thomey1992 <nguyenlinh@nanoco.com.vn>
2026-07-07 21:12:39 +0700

    Add excel.js for loading and processing Excel data

---
+++ js/excel.js
@@ -0,0 +1,107 @@
+// ======================================
+// NEMS Enterprise V3
+// excel.js
+// ======================================
+
+const EXCEL_FILE = "data/Quản lý thiết bị.xlsx";
+
+let equipmentData = [];
+let eventData = [];
+
+async function loadExcel() {
+
+    try {
+
+        showLoading();
+
+        const response = await fetch(
+            EXCEL_FILE + "?v=" + new Date().getTime()
+        );
+
+        if (!response.ok) {
+
+            throw new Error("Không đọc được file Excel");
+
+        }
+
+        const arrayBuffer =
+            await response.arrayBuffer();
+
+        const workbook =
+            XLSX.read(arrayBuffer, {
+                type: "array"
+            });
+
+        equipmentData =
+            XLSX.utils.sheet_to_json(
+                workbook.Sheets["Equipment_Register"],
+                {
+                    defval: ""
+                }
+            );
+
+        eventData =
+            XLSX.utils.sheet_to_json(
+                workbook.Sheets["Asset_Event_Log1"],
+                {
+                    defval: ""
+                }
+            );
+
+        afterLoadExcel();
+
+    }
+
+    catch (error) {
+
+        console.error(error);
+
+        alert("Không thể đọc file Excel.");
+
+    }
+
+}
+
+function afterLoadExcel() {
+
+    initDashboard();
+
+    initEquipment();
+
+    initHistory();
+
+}
+
+function showLoading() {
+
+    const equipmentBody =
+        document.getElementById("equipmentTableBody");
+
+    const eventBody =
+        document.getElementById("eventTableBody");
+
+    equipmentBody.innerHTML =
+
+        `<tr>
+
+            <td colspan="7">
+
+                Đang tải dữ liệu...
+
+            </td>
+
+        </tr>`;
+
+    eventBody.innerHTML =
+
+        `<tr>
+
+            <td colspan="6">
+
+                Đang tải dữ liệu...
+
+            </td>
+
+        </tr>`;
+
+}
